904cb8970f18764d6ea719edf870a5805c323843
Isaac Miti <ikayz@live.com>
2019-04-30 16:08:31 +0200

    Add adapters to control data management

--- app/src/main/java/com/ikayz/android/notekeeper/NoteActivity.java
+++ app/src/main/java/com/ikayz/android/notekeeper/NoteActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -20,6 +23,10 @@ public class NoteActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Spinner spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
     }
 
     @Override
