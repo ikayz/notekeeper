022810f73ff11bf79728f0b7741a61ea3d324bc7
Isaac Miti <ikayz@live.com>
2019-06-30 20:18:24 +0200

    Add intent to Note activity

--- app/src/main/java/com/ikayz/android/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/ikayz/android/notekeeper/NoteListActivity.java
@@ -47,6 +47,7 @@ public class NoteListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
                 NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(NoteActivity.NOTE_INFO, note);
                 startActivity(intent);
             }
         });
