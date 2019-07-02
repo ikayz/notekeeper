1107ae2067f9ea6ddec8a28bbc91132afd28d218
Isaac Miti <ikayz@live.com>
2019-07-02 16:12:01 +0200

    Refactor the spinner

--- app/src/main/java/com/ikayz/android/notekeeper/NoteActivity.java
+++ app/src/main/java/com/ikayz/android/notekeeper/NoteActivity.java
@@ -17,6 +17,7 @@ public class NoteActivity extends AppCompatActivity {
     public static final int POSITION_NOT_SET = -1;
     private NoteInfo mNote;
     private boolean mIsNewNote;
+    private Spinner mSpinnerCourses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +26,18 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Spinner spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+        mSpinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
         ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCourses.setAdapter(adapterCourses);
+        mSpinnerCourses.setAdapter(adapterCourses);
 
         readDisplayStateValues();
         EditText textNoteTitle = (EditText) findViewById(R.id.text_note_title);
         EditText textNoteText = (EditText) findViewById(R.id.text_note_text);
 
         if(!mIsNewNote)
-            displayNotes(spinnerCourses, textNoteTitle, textNoteText);
+            displayNotes(mSpinnerCourses, textNoteTitle, textNoteText);
     }
 
     private void displayNotes(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
