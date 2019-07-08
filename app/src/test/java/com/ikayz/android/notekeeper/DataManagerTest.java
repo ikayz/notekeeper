fc6463a1ca9b98d92db0b53400a298ba967b49b8
Isaac Miti <ikayz@live.com>
2019-07-08 22:11:12 +0200

    Add test note variables to class

--- app/src/test/java/com/ikayz/android/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/ikayz/android/notekeeper/DataManagerTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 public class DataManagerTest {
 
     @Test
-    public void createNewNote() {
+    public void createNewNote() throws Exception {
+        final CourseInfo course = DataManager.getInstance().getCourse("android_async");
+        final String noteTitle = "Text note title";
+        final String noteText = "This is the body text of the test note";
     }
 }
