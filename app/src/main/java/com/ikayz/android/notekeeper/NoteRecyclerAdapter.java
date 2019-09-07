6e4e37939113edd849d706c42b884f6df847557f
Isaac Miti <ikayz@live.com>
2019-09-07 23:46:51 +0200

    Add Note rcycler adapter

---
+++ app/src/main/java/com/ikayz/android/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,17 @@
+package com.ikayz.android.notekeeper;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/*
+Created by ikayz on 07/09/2019
+*/
+public class NoteRecyclerAdapter {
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
