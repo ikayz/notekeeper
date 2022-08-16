13e7383d7c3152c73a1547a4adb8f698e12945f0
Isaac Miti <ikayz@live.com>
2022-08-16 11:49:52 +0200

    Add initializer to run the initialize function

--- app/src/main/java/com/ikayz/kotlin/notekeeper/DataManager.kt
+++ app/src/main/java/com/ikayz/kotlin/notekeeper/DataManager.kt
@@ -3,4 +3,20 @@ package com.ikayz.kotlin.notekeeper
 class DataManager {
     val courses = HashMap<String, CourseInfo>()
     val notes = ArrayList<NoteInfo>()
+
+    // Runs automatically to execute code
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+    }
 }
